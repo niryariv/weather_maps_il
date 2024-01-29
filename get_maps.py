29e43dc57c03e1b931386b21e1647c630c68363c
niryariv <niryariv@gmail.com>
2024-01-29 11:20:18 +0200

    work with artifacts

--- get_maps.py
+++ get_maps.py
@@ -5,30 +5,20 @@ import os
 IMAGES_INFO = [
     {
         "url": "https://ims.gov.il/sites/default/files/ims_data/map_images/c3RainForecast/c3RainForecast.png",
-        "header_file": "last_modified_c3.txt"
+        "header_file": "data/last_modified_c3.txt"
     },
     {
         "url": "https://ims.gov.il/sites/default/files/ims_data/map_images/ecRainForecast/ecRainForecast.png",
-        "header_file": "last_modified_ec.txt"
+        "header_file": "data/last_modified_ec.txt"
     }
 ]
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')  # Get Telegram token from environment variable
-CHANNEL_ID = os.getenv('CHANNEL_ID')  # Get Channel ID from environment variable
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
 
-def check_for_update(url, header_file):
-    response = requests.head(url)
-    last_modified = response.headers.get('Last-Modified')
-
-    if not last_modified:
-        print(f"Last-Modified header not found for {url}.")
-        return True
-
-    if not os.path.exists(header_file) or open(header_file).read().strip() != last_modified:
-        with open(header_file, 'w') as file:
-            file.write(last_modified)
-        return True
-
-    return False
+def download_image(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
 
 def send_telegram_photo(bot_token, chat_id, photo_data):
     url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
@@ -37,12 +27,32 @@ def send_telegram_photo(bot_token, chat_id, photo_data):
     response = requests.post(url, files=files, data=data)
     response.raise_for_status()
 
+def check_for_update(image_info):
+    response = requests.head(image_info['url'])
+    last_modified = response.headers.get('Last-Modified')
+
+    if not last_modified:
+        print(f"Last-Modified header not found for {image_info['url']}.")
+        return True
+
+    previous_last_modified = None
+    if os.path.exists(image_info['header_file']):
+        with open(image_info['header_file'], 'r') as file:
+            previous_last_modified = file.read().strip()
+
+    if last_modified != previous_last_modified:
+        with open(image_info['header_file'], 'w') as file:
+            file.write(last_modified)
+        return True
+
+    return False
+
 def main():
     print("Script started. Checking for new content.")
     for image_info in IMAGES_INFO:
-        if check_for_update(image_info['url'], image_info['header_file']):
+        if check_for_update(image_info):
             print(f"New content detected for {image_info['url']}! Downloading image.")
-            image_data = requests.get(image_info['url']).content
+            image_data = download_image(image_info['url'])
             print(f"Sending image to Telegram from {image_info['url']}.")
             send_telegram_photo(TELEGRAM_TOKEN, CHANNEL_ID, image_data)
             print("Image sent successfully.")
